bfc96157c506b0d718b5c2d6bf53b821149ba092
Musa Waseem <musaw650@gmail.com>
2024-05-08 16:20:28 -0400

    adding book and modifying home

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 require("dotenv").config();
-const { MongoClient, ObjectId } = require("mongodb"); 
+const { MongoClient, ObjectId } = require("mongodb");
 process.stdin.setEncoding("utf8");
 const args = process.argv.slice(2);
 
@@ -13,154 +13,52 @@ app.set("views", "./templates");
 app.use(bodyParser.json());
 app.use(express.json());
 
-// app.get("/applications", (req, res) => {
-//   res.render("applications");
-// });
+const http = require('https');
 
+const options = {
+	method: 'GET',
+	hostname: 'sky-scrapper.p.rapidapi.com',
+	port: null,
+	path: '/api/v1/flights/getFlightDetails?itineraryId=%3CREQUIRED%3E&legs=%5B%7B%22destination%22%3A%22LOND%22%2C%22origin%22%3A%22LAXA%22%2C%22date%22%3A%222024-04-11%22%7D%5D&adults=1&currency=USD',
+	headers: {
+		'X-RapidAPI-Key': '0c583d7cb2msh81fa754ebbe153cp1b797djsn47c8d069c3a7',
+		'X-RapidAPI-Host': 'sky-scrapper.p.rapidapi.com'
+	}
+};
+
+// Log the request options
+console.log("Request options:", options);
+
+const req = http.request(options, function (res) {
+	// Log the response status and headers
+	console.log(`STATUS: ${res.statusCode}`);
+	console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+	const chunks = [];
+
+	res.on('data', function (chunk) {
+		// Log each chunk of data as it is received
+		console.log("Chunk received:", chunk);
+		chunks.push(chunk);
+	});
+
+	res.on('end', function () {
+		const body = Buffer.concat(chunks);
+		// Log the complete response body
+		console.log(`BODY: ${body.toString()}`);
+	});
+});
+
+req.on('error', (e) => {
+	// Log any errors that occur during the request
+	console.error(`Problem with request: ${e.message}`);
+});
+
+req.end();
+
+// Example endpoint to ensure your server is running
 app.get("/", (req, res) => {
   res.render("home");
 });
-
-// app.get("/view/:id", async (req, res) => {
-//     const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
-//     try {
-//         await client.connect();
-//         const db = client.db(process.env.MONGO_DB_NAME);
-//         const collection = db.collection(process.env.MONGO_COLLECTION);
-//         const application = await collection.findOne({ _id: new ObjectId(req.params.id) });
-//         if (application) {
-//             res.render("view", application); 
-//         } else {
-//             res.status(404).send("Application not found");
-//         }
-//     } catch (e) {
-//         console.error(e);
-//         res.status(500).send({ message: e.message });
-//     } finally {
-//         await client.close();
-//     }
-// });
-
-// app.get("/review", (req, res) => {
-//   res.render("review");
-// });
-
-// app.get("/app_not_found", (req, res) => {
-//         res.render("app_not_found");
-//       });
-      
-
-// app.get("/gpa", (req, res) => {
-//   res.render("gpa");
-// });
-
-// app.get("/remove", (req, res) => {
-//   res.render("remove");
-// });
-
-// app.get("/processGPA", (req, res) => {
-//         let gpaTable = decodeURIComponent(req.query.data);
-//         res.render("processGPA", { gpaTable: gpaTable });
-// });
-
-
-// app.get("/processRemove", (req, res) => {
-//         let numRemoved = req.query.data;
-//         res.render("processRemove", { numRemoved: numRemoved });
-// });
-
-// app.post("/remove-all", async (req, res) => {
-//         const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
-//         try {
-//             await client.connect();
-//             const db = client.db(process.env.MONGO_DB_NAME);
-//             const collection = db.collection(process.env.MONGO_COLLECTION);
-//             const result = await collection.deleteMany({});
-//             const numRemoved = result.deletedCount;
-//             console.log(`${numRemoved} records removed.`); 
-//             res.status(200).json({ numRemoved: numRemoved }); 
-//         } catch (e) {
-//             console.error("Error removing records:", e);
-//         } finally {
-//             await client.close();
-//         }
-//     });
-
-
-// app.post("/submit-application", async (req, res) => {
-//         const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
-//         try {
-//             await client.connect();
-//             const db = client.db(process.env.MONGO_DB_NAME);
-//             const collection = db.collection(process.env.MONGO_COLLECTION);
-//             const { name, email, gpa, background } = req.body;
-//             const numericGPA = parseFloat(gpa); 
-//             const result = await collection.insertOne({ name, email, gpa: numericGPA, background });
-//             if (result.insertedId) {
-//                 res.status(201).json({ message: "Application submitted successfully", id: result.insertedId });
-//             } else {
-//                 res.status(400).send("error submitting");
-//             }
-//         } catch (e) {
-//             console.error(e);
-//             res.status(500).send({ message: e.message });
-//         } finally {
-//             await client.close();
-//         }
-//     });
-    
-
-//     app.post("/get-application", async (req, res) => {
-//         const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
-//         const { email } = req.body; 
-//         try {
-//             await client.connect();
-//             const db = client.db(process.env.MONGO_DB_NAME);
-//             const collection = db.collection(process.env.MONGO_COLLECTION);
-//             const application = await collection.findOne({ email: email });
-    
-//             if (application) {
-//                 res.json({ id: application._id }); 
-//             }  else {
-//                 res.sendStatus(404);
-//             }
-//         } catch (e) {
-//             console.error(e);
-//             res.status(500).json({ message: e.message });
-//         } finally {
-//             await client.close();
-//         }
-//     });
-        
-
-//     app.post("/get-gpa", async (req, res) => {
-//         const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
-//         let gpaTable = "<p>No matching applications found.</p>"; 
-    
-//         try {
-//             await client.connect();
-//             const db = client.db(process.env.MONGO_DB_NAME);
-//             const collection = db.collection(process.env.MONGO_COLLECTION);
-//             const gpa = parseFloat(req.body.gpa);
-//             const applications = await collection.find({ gpa: { $gte: gpa } }).toArray();
-//             if (applications.length > 0) {
-//                 gpaTable = `<table border='1'><tr><th>Name</th><th>GPA</th></tr>`;
-//                 applications.forEach(applicant => {
-//                     const gpaNum = parseFloat(applicant.gpa); 
-//                     gpaTable += `<tr><td>${applicant.name}</td><td>${gpaNum.toFixed(2)}</td></tr>`; 
-//                 });
-//                 gpaTable += `</table>`;
-//             }
-//             res.render("processGPA", { gpaTable: gpaTable });
-//         } catch (e) {
-//             console.error(e);
-//         } finally {
-//             await client.close();
-//         }
-//     });
-    
-    
-    //testing
 
 app.listen(portNumber, () => {
   console.log(`Web server started and running at http://localhost:${portNumber}`);
